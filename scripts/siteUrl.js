a544526b277191ed846931f159f4af4bb86d7569
孙明喆 <sunmingzhe@sunmingzhedeMacBook-Pro.local>
2019-06-07 09:41:36 +0800

    调整图片展示尺寸

--- scripts/siteUrl.js
+++ scripts/siteUrl.js
@@ -3,5 +3,5 @@
 const config = hexo.config;
 
 hexo.extend.tag.register('img_path', function (args) {
-    return `<img src="${config.img_path + args[0]}" style="margin: 6px 0;width:${args[1] ? args[1]+'px' : "auto"};height:${args[2] ? args[2]+'px' : 'auto'}"/>`;
+    return `<img src="${config.img_path + args[0]}" style="margin: 6px 0;width:${args[1] ? args[1]+'px' : "auto"};height:${args[2] ? args[2]+'px' : 'auto'};max-width: 400px"/>`;
 });
